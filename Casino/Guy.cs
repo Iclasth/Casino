9d4e03b2bbd00f3cf74b1dbf73349e761a6a175a
iclasTH <iclasinprogramacao@gmail.com>
2025-10-20 13:27:18 -0300

    Adicionar arquivos de projeto.

---
+++ Casino/Guy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoGuysTrading
+{
+    public class Guy
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Cash { get; set; } 
+
+        public Guy(string name, int cash)
+        {
+            Name = name;
+            Cash = cash;
+        }
+
+        /// <summary>
+        /// A simple method to write out the guy's info (his name and cash).
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"Hi! i'm {Name} and i have {Cash} bucks (dolar).");
+        }
+
+        /// <summary>
+        /// Withdraws a specified amount of cash from the available balance.
+        /// </summary>
+        /// <remarks>If the requested amount is less than or equal to 0, or if it exceeds the available
+        /// cash balance, the method will return 0 and no cash will be withdrawn.</remarks>
+        /// <param name="amount">The amount of cash to withdraw. Must be greater than 0 and less than or equal to the available cash balance.</param>
+        /// <returns>The amount of cash successfully withdrawn. Returns 0 if the requested amount is invalid or exceeds the
+        /// available balance.</returns>
+        public double GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount I'll take.");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount} bucks.");
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        /// <summary>
+        /// Adds the specified amount of cash to the current balance.
+        /// </summary>
+        /// <remarks>If the specified <paramref name="amount"/> is less than or equal to 0, the method
+        /// will not modify the balance and will return 0. Otherwise, the specified amount is added to the current
+        /// balance.</remarks>
+        /// <param name="amount">The amount of cash to add. Must be greater than 0.</param>
+        /// <returns>The amount of cash successfully added. Returns 0 if the specified amount is less than or equal to 0.</returns>
+        public void ReciveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount that i can take.");
+                
+            }
+            Cash += amount;
+            
+        }
+    }
+}
---
+++ Casino/Program.cs
@@ -0,0 +1,67 @@
+﻿using TwoGuysTrading;
+
+Random random = new Random();
+double odds = 0.75;
+Guy player = new Guy("The Player", 100);
+
+Console.WriteLine("Welcome to Paulista's Casino!");
+Console.WriteLine("------------------------------");
+Console.WriteLine("Do you wanna play?");
+
+string? answer = Console.ReadLine();
+
+if (answer?.ToLower() != "yes")
+{
+    Console.WriteLine("Okay, maybe next time!");
+    return;
+
+}
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Welcome to the game! The odds is 0.75");
+    Console.WriteLine($"{player.Name} have {player.Cash} bucks");
+    Console.WriteLine("How much do you wanna bet?");
+    string? betAnswer = Console.ReadLine();
+    
+    if (betAnswer == null)
+    {
+        Console.WriteLine("Invalid bet amount.");
+        Console.WriteLine("How much do you wanna bet?");
+        betAnswer = Console.ReadLine();
+    }
+
+    if (int.TryParse(betAnswer, out int betAmount))
+    {
+        int cashGiven = (int)player.GiveCash(betAmount);
+        int pot = cashGiven * 2;
+
+        if (cashGiven == 0)
+        {
+            Console.WriteLine("You don't have enough cash to make that bet.");
+            return;
+
+        }
+        if (random.NextDouble() < odds)
+        {
+            Console.WriteLine("You win!");
+            player.ReciveCash(pot);
+        }
+        else
+        {
+            Console.WriteLine("You lose!");
+            player.GiveCash(betAmount);
+        }
+
+        Console.WriteLine("Do you wanna play again? (yes/no)");
+        string? playAgainAnswer = Console.ReadLine();
+
+        if(playAgainAnswer?.ToLower() != "yes")
+        {
+            Console.WriteLine("Thanks for playing! See you next time.");
+            break;
+        }
+    }
+
+}
